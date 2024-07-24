56dd638472ac94677b99164d6a8f1a5273c95f8e
ZiZouet <91062845+zizouet@users.noreply.github.com>
2024-07-25 01:59:39 +0200

    fix: the most okay

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 """ Main module to run the program. """
 
-from data_analysis import data_analysis as data_analysis
-from input_handling import loading_input as loading_input
+from data_analysis import data_analysis as da
+from input_handling import loading_input as li
 
 
 def main():
@@ -9,13 +9,13 @@ def main():
     main function to run the program.
     """
     # load data
-    df = loading_input.loading_data_from("data/MesDatas")
+    df = li.loading_data_from("data/MesDatas")
     # check if data is loaded
     if df is None:
         print("No data to analyze")
         return
     # call routine to analyze the data
-    data_analysis.full_analysis(df)
+    da.full_analysis(df)
 
 
 if __name__ == "__main__":
